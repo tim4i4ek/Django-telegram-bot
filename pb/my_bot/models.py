b8e7678af5dad522bc69b00768cdba5e885b97ab
Тимофей Яровик <timofey2009yarovik@gmail.com>
2026-06-02 15:28:40 +0300

    True

--- pb/my_bot/models.py
+++ pb/my_bot/models.py
@@ -71,7 +71,7 @@ class Appointment(models.Model):
     proposition = models.ForeignKey(Work, on_delete=models.CASCADE, related_name='appointments')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     client_nickname = models.CharField(max_length=100)
-    is_approved = models.BooleanField(default=False)
+    is_approved = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.client_name} - {self.date} {self.time_slot}:00"
